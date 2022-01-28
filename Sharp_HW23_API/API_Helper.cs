5251905350371fe29559b3e8cea218676772e7d2
aerosmith_dp <aerosmith.dp@gmail.com>
2022-01-28 02:52:59 +0200

    upload

---
+++ Sharp_HW23_API/API_Helper.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using RestSharp;
+using OpenQA.Selenium;
+
+namespace Sharp_HW23_API
+{
+    public static class API_Helper
+    {
+        public static IRestResponse SendApiRequest(object body, Dictionary<string, string> headers, string link, Method type)
+        {
+            RestClient client = new RestClient(link)
+            {
+                Timeout = 300000
+            };
+
+            RestRequest request = new RestRequest(type);
+            foreach (var header in headers)
+            {
+                request.AddHeader(header.Key, header.Value);
+            }
+
+            bool isBodyJson = false;
+            foreach (var header in headers)
+            {
+                if (header.Value.Contains("application/json"))
+                {
+                    isBodyJson = true;
+                    break;
+                }
+            }
+            if (!isBodyJson)
+            {
+                foreach (var data in (Dictionary<string, string>)body)
+                {
+                    request.AddParameter(data.Key, data.Value);
+                }
+            }
+            else
+            {
+                request.AddJsonBody(body);
+                request.RequestFormat = DataFormat.Json;
+            }
+
+            IRestResponse response = client.Execute(request);
+
+            return response;
+        }
+
+        public static Cookie ExtractCookie(IRestResponse response, string cookieName)
+        {
+            Cookie result = null;
+            foreach (var cookie in response.Cookies)
+                if (cookie.Name.Equals(cookieName))
+                    result = new Cookie(cookie.Name, cookie.Value, cookie.Domain, cookie.Path, null);
+
+            return result;
+        }
+
+        public static List<Cookie> ExtractAllCookies(IRestResponse response)
+        {
+            List<Cookie> result = new List<Cookie>();
+            foreach (var cookie in response.Cookies)
+                result.Add(new Cookie(cookie.Name, cookie.Value, cookie.Domain, cookie.Path, null));
+
+            return result;
+        }
+
+    }
+}
---
+++ Sharp_HW23_API/UnitTest1.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using RestSharp;
+using OpenQA.Selenium;
+
+
+using Xunit;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.IO;
+
+namespace Sharp_HW23_API
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var body = new Dictionary<string, string>
+            {
+                { "ulogin", "art1613122"},
+                { "upassword", "505558545"}
+            };
+
+            var headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "application/x-www-form-urlencoded"}
+            };
+
+            IRestResponse response = API_Helper.SendApiRequest(body, headers, "https://my.soyuz.in.ua", Method.POST);
+
+            var cookie = API_Helper.ExtractCookie(response, "zbs_lang");
+            var cookie2 = API_Helper.ExtractCookie(response, "ulogin");
+            var cookie3 = API_Helper.ExtractCookie(response, "upassword");
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://my.soyuz.in.ua");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Cookies.AddCookie(cookie);
+            driver.Manage().Cookies.AddCookie(cookie2);
+            driver.Manage().Cookies.AddCookie(cookie3);
+            driver.Navigate().GoToUrl("https://my.soyuz.in.ua/index.php");
+
+            var headers2 = new Dictionary<string, string>
+            {
+                { "Cookie", cookie.Name},
+                { "Content-Type", "application/x-www-form-urlencoded"}
+            };
+
+            IRestResponse response2 = API_Helper.SendApiRequest(body, headers2, "https://my.soyuz.in.ua", Method.GET);
+
+            driver.Quit();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var client = new RestClient("https://clip2net.com");
+            var request = new RestRequest(Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader("Content-Type", "image/jpeg");
+            request.AddFile("content", "/pic.jpg");
+            IRestResponse response = client.Execute(request);
+            Assert.Equal("OK", response.StatusCode.ToString());
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var client = new RestClient("https://clip2net.com/clip/m566236/jjjk3/1643326942-f042f-6kb.jpg");
+            var request = new RestRequest(Method.GET);
+            byte[] downloaded = client.DownloadData(request);
+            File.WriteAllBytes(Path.Combine("C:/Users/User/Desktop", "nyancat.jpg"), downloaded);
+        }
+
+
+    }
+}
